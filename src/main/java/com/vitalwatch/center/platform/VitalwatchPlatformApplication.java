3134aac8c1757aa56725499fba9b51203e7b8d40
TheCap213 <jyonel295@gmail.com>
2026-07-05 16:45:08 -0500

    chore(config): configure dev profile with environment variables

---
+++ src/main/java/com/vitalwatch/center/platform/VitalwatchPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.vitalwatch.center.platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VitalwatchPlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VitalwatchPlatformApplication.class, args);
+    }
+
+}
