b3bd5c1d904214e021236b9828472f2b4eeab525
TheCap213 <jyonel295@gmail.com>
2026-07-05 23:24:05 -0500

    fix(iam): provide noop invitation email service when resend is disabled

--- src/main/java/com/vitalwatch/center/platform/iam/invitations/infrastructure/email/noop/NoOpInvitationEmailService.java
+++ src/main/java/com/vitalwatch/center/platform/iam/invitations/infrastructure/email/noop/NoOpInvitationEmailService.java
@@ -2,14 +2,18 @@ package com.vitalwatch.center.platform.iam.invitations.infrastructure.email.noop
 
 import com.vitalwatch.center.platform.iam.invitations.application.internal.outboundservices.InvitationEmailService;
 import com.vitalwatch.center.platform.iam.invitations.infrastructure.persistence.jpa.entities.InvitationJpaEntity;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.stereotype.Service;
 
 /**
  * No-operation email service used when real email delivery is disabled.
  */
 @Service
-@ConditionalOnMissingBean(InvitationEmailService.class)
+@ConditionalOnProperty(
+        name = "application.email.resend.enabled",
+        havingValue = "false",
+        matchIfMissing = true
+)
 public class NoOpInvitationEmailService implements InvitationEmailService {
 
     @Override
