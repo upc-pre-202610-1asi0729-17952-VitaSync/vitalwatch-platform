f1bf530fce936a9a3e35f037429735f687cbf180
TheCap213 <jyonel295@gmail.com>
2026-07-05 20:12:27 -0500

    chore(docs): add bearer authentication to openapi

--- src/main/java/com/vitalwatch/center/platform/shared/infrastructure/documentation/openapi/configuration/OpenApiConfiguration.java
+++ src/main/java/com/vitalwatch/center/platform/shared/infrastructure/documentation/openapi/configuration/OpenApiConfiguration.java
@@ -2,9 +2,12 @@ package com.vitalwatch.center.platform.shared.infrastructure.documentation.opena
 
 import io.swagger.v3.oas.models.ExternalDocumentation;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -13,10 +16,12 @@ import org.springframework.context.annotation.Configuration;
 import java.util.List;
 
 /**
- * OpenAPI configuration for VitalWatch Platform API documentation.
+ * OpenAPI documentation configuration.
  */
 @Configuration
 public class OpenApiConfiguration {
+
+    private static final String BEARER_AUTH_SCHEME = "bearerAuth";
 
     @Value("${documentation.server.url:http://localhost:8080/api/v1}")
     private String serverUrl;
@@ -37,6 +42,17 @@ public class OpenApiConfiguration {
                         .license(new License()
                                 .name("Apache 2.0")
                                 .url("https://www.apache.org/licenses/LICENSE-2.0.html")))
+                .components(new Components()
+                        .addSecuritySchemes(
+                                BEARER_AUTH_SCHEME,
+                                new SecurityScheme()
+                                        .name(BEARER_AUTH_SCHEME)
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")
+                        ))
+                .addSecurityItem(new SecurityRequirement()
+                        .addList(BEARER_AUTH_SCHEME))
                 .externalDocs(new ExternalDocumentation()
                         .description("VitalWatch Repository")
                         .url("https://github.com/upc-pre-202610-1asi0729-17952-VitaSync/vitalwatch-platform"));
